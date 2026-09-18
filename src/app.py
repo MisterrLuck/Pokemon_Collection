b60436c35f98a56177b8fffdc978a957add5ab71
joshua <joshuacarpenter08@gmail.com>
2026-09-18 00:23:55 -0400

    Added home page and new card creation page. Started the css file

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 # The main entry point to the app
+# url_for is for the function
 
-from flask import Flask, request, render_template, make_response
+from flask import Flask, request, render_template, make_response, redirect, abort, url_for
 import os
 from database import db_session
 
@@ -12,28 +13,31 @@ def shutdown_session(exception=None):
 
 @app.route("/")
 def hello_world():
-    return render_template("base.html")
+    return redirect(url_for("login"))
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/new", methods=["GET", "POST"])
+def new_card():
+    return render_template("new_card.html")
 
 @app.route("/hello")
 @app.route("/hello/<name>")
 def hello(name=None):
-    return render_template("test.html", person=name)
+    return render_template("hello.html", person=name)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     error = None
 
-    #user_cookie = request.cookies.get("username")
-
     # Submitted login details
-    if request.method == "POST": # or user_cookie:
-        #user = user_cookie if user_cookie else request.form["username"]
+    if request.method == "POST":
         user = request.form["username"]
-        if user in ["Lizzy", "Josh"]:
-            temp = render_template("test.html", username=user)
-            #resp = make_response(temp)
-            #resp.set_cookie("username", user)
-            #return resp
+        if user in ["Lizzy", "Josh", "Victoria"]:
+            # I want to change this to use url_for or smth
+            temp = render_template("hello.html", username=user)
             return temp
         else:
             error = "Invalid username"
--- src/models.py
+++ src/models.py
@@ -6,7 +6,7 @@ from database import Base
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    name = Column(String(80), unique=True, nullable=False)
+    name = Column(String(20), unique=True, nullable=False)
     #password_hash = Column(String(256), nullable=False)
 
     def __init__(self, name=None):
@@ -19,6 +19,7 @@ class Card(Base):
     __tablename__ = "cards"
     id = Column(Integer, primary_key=True)
     name = Column(String(20))
+    user = Column(String(20))
     # type: full art, holo, reverse holo, regular, ex
     # number out of number
     first_num = Column(Integer)
@@ -27,7 +28,7 @@ class Card(Base):
     count = Column(Integer)
     # image - add a web scraper that searches for the images based on the information
 
-    def __init__(self, name=None, first=0, second=0, count=1):
+    def __init__(self, name=None, user=None, first=0, second=0, count=1):
         self.name = name
         self.count = count
         self.first_num = first
